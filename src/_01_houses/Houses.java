8287735dfb301fd14cca26d82cb1563ec967c4c6
olive <olive@DESKTOP-0SN426D>
2020-09-07 13:26:00 -0700

    houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -16,23 +16,23 @@ public class Houses{
 		a.moveTo(800,500);
 		a.turn(-90);
 		for (int i = 0; i < 11; i++) {
-			
-		
-		drawHouse(a, 20);
+		String timHeight = JOptionPane.showInputDialog("How tall should this house be; Small, Medium, or Large?");
+		int timInt = Integer.parseInt(timHeight);
+		drawHouse(a, timInt);
 		}
 	}
-	public void drawHouse(Robot tim, int height) {
+	public void drawHouse(Robot tim, int timInt) {
 		
 		
 		tim.setPenColor(0, 102, 0);
 		tim.move(20);
 		tim.turn(90);
 		tim.setPenColor(Color.green);
-		tim.move(height);
+		tim.move(timInt);
 		tim.turn(-90);
 		tim.move(35);
 		tim.turn(-90);
-		tim.move(height);
+		tim.move(timInt);
 		tim.turn(90);
 		tim.setPenColor(0, 102, 0);
 		tim.move(20);
