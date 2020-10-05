2b1e5c0f7f2f1a9206a88b8211e86a6d1a420a83
olive <olive@DESKTOP-0SN426D>
2020-10-05 13:26:00 -0700

    poggers

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -10,23 +10,25 @@ public class Houses {
 
 	public void run() {
 
-		Robot a = new Robot();
-		a.setSpeed(50);
-		a.penDown();
-		a.moveTo(800, 500);
-		a.turn(-90);
+		Robot tim = new Robot();
+		tim.setSpeed(50);
+		tim.penDown();
+		tim.moveTo(800, 500);
+		tim.turn(-90);
 		for (int i = 0; i < 11; i++) {
 			String timHeight = JOptionPane.showInputDialog("How tall should this house be; Small, Medium, or Large?");
-drawBuild(a, timHeight);
+			String timColor = JOptionPane.showInputDialog("What color should this house be? Red, Green, or Blue?");
+
+			drawBuild(tim, timHeight, timColor);
 		}
 	}
 
-	public void drawHouse(Robot tim, int timTall) {
+	public void drawHouse(Robot tim, int timTall, String timColor) {
 
 		tim.setPenColor(0, 102, 0);
 		tim.move(20);
 		tim.turn(90);
-		tim.setPenColor(Color.green);
+		chooseColor(tim, timColor);
 		tim.move(timTall);
 		tim.turn(-90);
 		tim.move(35);
@@ -38,14 +40,26 @@ drawBuild(a, timHeight);
 
 	}
 
-	public void drawBuild(Robot tim, String timHeight) {
+	public void drawBuild(Robot tim, String timHeight, String timColor) {
 		timHeight = timHeight.trim();
 		if (timHeight.equalsIgnoreCase("small")) {
-			drawHouse(tim, 60);
+			drawHouse(tim, 60, timColor);
 		} else if (timHeight.equalsIgnoreCase("medium")) {
-			drawHouse(tim, 120);
+			drawHouse(tim, 120, timColor);
 		} else if (timHeight.equalsIgnoreCase("large")) {
-			drawHouse(tim, 250);
+			drawHouse(tim, 250, timColor);
+		}
+
+	}
+
+	public void chooseColor(Robot tim, String timColor) {
+		timColor = timColor.trim();
+		if (timColor.equalsIgnoreCase("Blue")) {
+			tim.setPenColor(Color.blue);
+		} else if (timColor.equalsIgnoreCase("Red")) {
+			tim.setPenColor(Color.red);
+		} else if (timColor.equalsIgnoreCase("Green")) {
+			tim.setPenColor(Color.green);
 		}
 
 	}
